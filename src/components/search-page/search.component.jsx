299f6c7289143e5e3fe1db3240ce3c79748d7e0e
mazenhoballah <mazen.hoballah.mh@gmail.com>
2021-07-30 17:22:00 +0300

    fixing login issues

--- src/components/search-page/search.component.jsx
+++ src/components/search-page/search.component.jsx
@@ -4,7 +4,6 @@ import { get } from '../../utils/api';
 import './search-page.styles.scss';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -37,7 +36,6 @@ const SearchPage = (props) => {
         const searchTerm = event.target.value;
         setSearchTerm(searchTerm);
     };
-
     const searchForItems = async () => {
         setItems({});
         try {
@@ -45,7 +43,9 @@ const SearchPage = (props) => {
             const API_URL = `https://api.spotify.com/v1/search?q=${term.toLocaleUpperCase()}&type=artist`;
             const result = await get(API_URL);
             setItems(result);
-        } catch (error) {}
+        } catch (error) {
+
+        }
     };
 
     useEffect(() => {
