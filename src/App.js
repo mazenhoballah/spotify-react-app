d5f4bf04413056b39e3d40db58efcd2951266c67
mazenhoballah <53557473+mazenhoballah@users.noreply.github.com>
2021-09-24 03:37:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import RedirectPage from './components/redirect-page/redirect.component';
 import NotFoundPage from './components/not-found-page/notFound.component';
 import SearchPage from './components/search-page/search.component';
 import AlbumsPage from './components/albums-page/albmus-page.component';
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
     return (
         <div className='App'>
+          <HashRouter>
             <Switch>
                 <Route exact path='/spotify-react-app' component={Login} />
                 <Route exact path='/spotify-react-app/redirect' component={RedirectPage} />
@@ -17,6 +18,7 @@ function App() {
                 <Route exact path='/spotify-react-app/:artistId/albums' component={AlbumsPage} />
                 <Route component={NotFoundPage} />
             </Switch>
+          </HashRouter>
         </div>
     );
 }
